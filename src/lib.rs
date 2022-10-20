b10a9aceda6cff027aedd24613d9323819cd4ca8
João Vitor Cunha Assumpção <joao.assumpo@gmail.com>
2022-10-20 12:47:41 +0000

    new() and push()

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
-pub mod list;
-use list::List;
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+struct Node {
+    item: usize,
+    next: Link,
+}
+
+enum Link {
+    Nil,
+    Some(Box<Node>),
+}
 
 
 impl List {
-    todo!();
+    pub fn new() -> Self {
+        List { head: Link::Nil }
+    }
+
+    pub fn push(&mut self, item: usize) {
+        self.head = Link::Some( 
+            Box::new( Node {
+                item,
+                next: mem::replace(&mut self.head, Link::Nil),
+        }))
+    }
 }
